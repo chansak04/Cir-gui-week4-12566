1e64af48a8d413b012af2bebd9833d2188509609
chansak04 <deawchansak@gmail.com>
2023-06-06 13:30:24 +0700

    first commit

---
+++ bin/dart_list.dart
@@ -0,0 +1,8 @@
+void main(){
+  List<String> vnl2023 = ["Thailand", "Japan", "Korean"];
+
+  for(int index = 0; index < vnl2023.length; index++) {
+    print(vnl2023[index]);
+  }
+
+}
---
+++ bin/dart_week4.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<int> number = [10,20,30,40,];
+  for(int i = 0; i < number.length; i++) {
+    print(number[i]);
+  }
+  //print(number[0]);
+  //print(number[1]);
+  //print(number[2]);
+  //print(number[3]);
+}
